323a5305e166847c69bbc9d268495c1cf2c9e915
Louis Ouellet <louis@TheMacSwitch.local>
2021-09-28 15:03:00 -0400

    UPDATE

--- api.php
+++ api.php
@@ -3,8 +3,21 @@ class pluginsAPI extends API {
 	public function status($request, $data){
 		if(isset($data)){
 			if(!is_array($data)){ $data = json_decode($data, true); }
-      $this->Settings['plugins'][$data['plugin']] = $data['state'];
+      $this->Settings['plugins'][$data['plugin']]['status'] = $data['state'];
       $this->SaveCfg(['plugins' => $this->Settings['plugins']]);
+      if($data['state']){
+        return [
+          "success" => $this->Language->Field["Plugin was activated"],
+          "request" => $request,
+          "data" => $data,
+        ];
+      } else {
+        return [
+          "success" => $this->Language->Field["Plugin was deactivated"],
+          "request" => $request,
+          "data" => $data,
+        ];
+      }
     }
   }
 }
