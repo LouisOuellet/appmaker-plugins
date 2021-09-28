5b83a7e0f5f6f49f481b1d35871746b87c5af31e
Louis Ouellet <louis@TheMacSwitch.local>
2021-09-28 14:56:53 -0400

    UPDATE

--- api.php
+++ api.php
@@ -3,7 +3,8 @@ class pluginsAPI extends API {
 	public function status($request, $data){
 		if(isset($data)){
 			if(!is_array($data)){ $data = json_decode($data, true); }
-      $this->SaveCfg(['plugins' => [$data['plugin'] => $data['state']]]);
+      $this->Settings['plugins'][$data['plugin']] = $data['state'];
+      $this->SaveCfg(['plugins' => $this->Settings['plugins']]);
     }
   }
 }
