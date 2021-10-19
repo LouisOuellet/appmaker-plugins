0fe62b73fdc7e988d2f7eb9576566c0b9b29d66c
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-19 10:12:28 -0400

    21.10-dev-98

--- dist/js/script.js
+++ dist/js/script.js
@@ -134,15 +134,24 @@ API.Plugins.plugins = {
 						content.find('input[type="checkbox"]').each(function(){
 							if(API.Helper.isSet(API,['Contents','Settings','plugins',$(this).attr('data-key'),'status'])){
 								$('input[type="checkbox"][data-key="'+$(this).attr('data-key')+'"]').prop( "checked",dataset.output.settings[$(this).attr('data-key')].status);
-								$('input[type="checkbox"][data-key="'+$(this).attr('data-key')+'"]').bootstrapSwitch('state',dataset.output.settings[$(this).attr('data-key')].status);
-							} else { $('input[type="checkbox"][data-key="'+$(this).attr('data-key')+'"]').bootstrapSwitch('state',false); }
-							$(this).bootstrapSwitch({
-								onSwitchChange:function(e,state){
-									console.log($(this).attr('data-key') + "Triggered");
-									dataset.output.settings[$(this).attr('data-key')].status = state;
-									API.request('plugins','status',{data:{plugin:$(this).attr('data-key'),state:state}});
-								}
-							});
+								$('input[type="checkbox"][data-key="'+$(this).attr('data-key')+'"]').bootstrapSwitch({
+									state:dataset.output.settings[$(this).attr('data-key')].status,
+									onSwitchChange:function(e,state){
+										console.log($(this).attr('data-key') + "Triggered");
+										dataset.output.settings[$(this).attr('data-key')].status = state;
+										API.request('plugins','status',{data:{plugin:$(this).attr('data-key'),state:state}});
+									}
+								});
+							} else {
+								$('input[type="checkbox"][data-key="'+$(this).attr('data-key')+'"]').bootstrapSwitch({
+									state:false,
+									onSwitchChange:function(e,state){
+										console.log($(this).attr('data-key') + "Triggered");
+										dataset.output.settings[$(this).attr('data-key')].status = state;
+										API.request('plugins','status',{data:{plugin:$(this).attr('data-key'),state:state}});
+									}
+								});
+							}
 						});
 					});
 				}
