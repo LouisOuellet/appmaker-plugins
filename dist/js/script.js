6f27dd404962c2e5efb032df09660ed39c09f38e
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-20 16:07:35 -0400

    21.10-dev-114

--- dist/js/script.js
+++ dist/js/script.js
@@ -139,7 +139,8 @@ API.Plugins.plugins = {
 								$(this).bootstrapSwitch({
 									state:dataset.output.settings[plugin].status,
 									onSwitchChange:function(e,state){
-										plugin = e.target.attr('data-key');
+										console.log(e);
+										plugin = $(this).attr('data-key');
 										console.log(plugin);
 										// dataset.output.settings[plugin].status = state;
 										// API.request('plugins','status',{data:{plugin:plugin,state:state}});
@@ -149,8 +150,8 @@ API.Plugins.plugins = {
 								$(this).bootstrapSwitch({
 									state:false,
 									onSwitchChange:function(e,state){
-										plugin = e.target.attr('data-key');
 										console.log(e);
+										plugin = $(this).attr('data-key');
 										console.log(plugin);
 										// dataset.output.settings[plugin].status = state;
 										// API.request('plugins','status',{data:{plugin:plugin,state:state}});
