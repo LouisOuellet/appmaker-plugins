8ac6740437ccfc29fb676e31801dd2ecd91278eb
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-21 15:59:12 -0400

    21.10-dev-130

--- dist/js/script.js
+++ dist/js/script.js
@@ -105,7 +105,6 @@ API.Plugins.plugins = {
 									API.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
 										json = JSON.parse(result);
 										if(json.success != undefined){
-											$.getScript('/plugins/'+json.data.plugin+'/dist/js/script.js');
 											$('[data-key='+json.data.plugin+'][data-action="install"]').hide();
 											$('[data-key='+json.data.plugin+'][data-action="uninstall"]').show();
 											$('[data-key='+json.data.plugin+'][data-status]').show();
