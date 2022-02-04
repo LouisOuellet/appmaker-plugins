cf0593f3387393ee02da4fa928d35c06b1bc3898
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-04 16:21:41 -0500

    22.02-dev-202

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,25 @@
-API.Plugins.plugins = {
+Engine.Plugins.plugins = {
 	init:function(){
-		API.GUI.Sidebar.Nav.add('Plugins', 'administration');
+		Engine.GUI.Sidebar.Nav.add('Plugins', 'administration');
 	},
 	load:{
 		index:function(){
-			API.request('plugins','get',{data:{}},function(result) {
+			Engine.request('plugins','get',{data:{}},function(result) {
 				var dataset = JSON.parse(result);
 				if(dataset.success != undefined){
-					API.Contents.Plugins = dataset.output.plugins;
-					API.Contents.Settings.plugins = dataset.output.settings;
-					API.Builder.card($('#pagecontent'),{ title: 'Plugins', icon: 'plugins'}, function(card){
+					Engine.Contents.Plugins = dataset.output.plugins;
+					Engine.Contents.Settings.plugins = dataset.output.settings;
+					Engine.Builder.card($('#pagecontent'),{ title: 'Plugins', icon: 'plugins'}, function(card){
 						var html = '', checked = '', content = card.find('.card-body');
 						html += '<div class="row">';
 							html += '<div class="col-6">';
-								html += '<h3>'+API.Contents.Language['Plugins']+'</h3>';
+								html += '<h3>'+Engine.Contents.Language['Plugins']+'</h3>';
 							html += '</div>';
 							html += '<div class="col-6">';
 								html += '<div class="input-group">';
 									html += '<input type="text" id="plugin_search" class="form-control">';
-									html += '<div class="input-group-append"><span class="input-group-text"><i class="icon icon-search mr-1"></i>'+API.Contents.Language['Search']+'</span></div>';
-									html += '<div class="input-group-append"><button class="btn btn-success" data-action="updateAll"><i class="fas fa-file-download mr-1"></i>'+API.Contents.Language['Update All']+'</button></div>';
+									html += '<div class="input-group-append"><span class="input-group-text"><i class="icon icon-search mr-1"></i>'+Engine.Contents.Language['Search']+'</span></div>';
+									html += '<div class="input-group-append"><button class="btn btn-success" data-action="updateAll"><i class="fas fa-file-download mr-1"></i>'+Engine.Contents.Language['Update All']+'</button></div>';
 								html += '</div>';
 							html += '</div>';
 						html += '</div>';
@@ -27,30 +27,30 @@ API.Plugins.plugins = {
 							for(var [plugin, status] of Object.entries(dataset.output.plugins)){
 								html += '<div class="col-12 py-2">';
 									html += '<div class="input-group">';
-										html += '<div class="input-group-prepend"><span class="input-group-text"><i class="icon icon-'+plugin+' mr-1"></i>'+API.Helper.ucfirst(API.Helper.clean(plugin))+'</span></div>';
-										if(API.Helper.isSet(API,['Contents','Settings','plugins',plugin,'version'])){
+										html += '<div class="input-group-prepend"><span class="input-group-text"><i class="icon icon-'+plugin+' mr-1"></i>'+Engine.Helper.ucfirst(Engine.Helper.clean(plugin))+'</span></div>';
+										if(Engine.Helper.isSet(Engine,['Contents','Settings','plugins',plugin,'version'])){
 											html += '<div class="input-group-append">';
 												html += '<div class="input-group-text">'+dataset.output.settings[plugin].version+'</div>';
 											html += '</div>';
 										}
-										if(API.Helper.isSet(API,['Contents','Settings','plugins',plugin,'build'])){
+										if(Engine.Helper.isSet(Engine,['Contents','Settings','plugins',plugin,'build'])){
 											html += '<div class="input-group-append">';
 												html += '<div class="input-group-text">'+dataset.output.settings[plugin].build+'</div>';
 											html += '</div>';
 										}
 										html += '<input type="text" class="form-control switch-spacer" disabled>';
 										html += '<div class="input-group-append" data-key="'+plugin+'" data-status="">';
-											html += '<div class="input-group-text"><i class="fas fa-puzzle-piece mr-1"></i>'+API.Contents.Language['Status']+'</div>';
+											html += '<div class="input-group-text"><i class="fas fa-puzzle-piece mr-1"></i>'+Engine.Contents.Language['Status']+'</div>';
 										html += '</div>';
 										html += '<div class="input-group-append" data-key="'+plugin+'" data-toggle-status="">';
 											html += '<div class="input-group-text p-1">';
-												html += '<input type="checkbox" data-key="'+plugin+'" name="'+plugin+'" title="'+API.Helper.ucfirst(API.Helper.clean(plugin))+'">';
+												html += '<input type="checkbox" data-key="'+plugin+'" name="'+plugin+'" title="'+Engine.Helper.ucfirst(Engine.Helper.clean(plugin))+'">';
 											html += '</div>';
 										html += '</div>';
 										html += '<div class="input-group-append">';
-											html += '<button type="button" data-key="'+plugin+'" data-action="update" class="btn btn-success" style="display:none;"><i class="fas fa-file-download mr-1"></i>'+API.Contents.Language['Update']+'</button>';
-											html += '<button type="button" data-key="'+plugin+'" data-action="uninstall" class="btn btn-danger" style="display:none;"><i class="fas fa-trash-alt mr-1"></i>'+API.Contents.Language['Uninstall']+'</button>';
-											html += '<button type="button" data-key="'+plugin+'" data-action="install" class="btn btn-success" style="display:none;"><i class="fas fa-download mr-1"></i>'+API.Contents.Language['Install']+'</button>';
+											html += '<button type="button" data-key="'+plugin+'" data-action="update" class="btn btn-success" style="display:none;"><i class="fas fa-file-download mr-1"></i>'+Engine.Contents.Language['Update']+'</button>';
+											html += '<button type="button" data-key="'+plugin+'" data-action="uninstall" class="btn btn-danger" style="display:none;"><i class="fas fa-trash-alt mr-1"></i>'+Engine.Contents.Language['Uninstall']+'</button>';
+											html += '<button type="button" data-key="'+plugin+'" data-action="install" class="btn btn-success" style="display:none;"><i class="fas fa-download mr-1"></i>'+Engine.Contents.Language['Install']+'</button>';
 										html += '</div>';
 									html += '</div>';
 								html += '</div>';
@@ -59,20 +59,20 @@ API.Plugins.plugins = {
 						content.html(html);
 						for(var [plugin, conf] of Object.entries(dataset.output.plugins)){
 							$.ajax({
-				        url: dataset.output.plugins[plugin].repository.host.raw+dataset.output.plugins[plugin].repository.name+'/'+API.Contents.Settings.repository.branch+dataset.output.plugins[plugin].repository.manifest,
+				        url: dataset.output.plugins[plugin].repository.host.raw+dataset.output.plugins[plugin].repository.name+'/'+Engine.Contents.Settings.repository.branch+dataset.output.plugins[plugin].repository.manifest,
 				        success: function(data){
 									thisplugin = this.url.substring(this.url.indexOf("appmaker-") + 9).split('/')[0];
 									var manifest = JSON.parse(data);
-									if(API.Helper.isSet(API,['Contents','Settings','plugins',thisplugin])){
+									if(Engine.Helper.isSet(Engine,['Contents','Settings','plugins',thisplugin])){
 										$('[data-key='+thisplugin+'][data-action="install"]').hide();
 										$('[data-key='+thisplugin+'][data-action="uninstall"]').show();
 										$('[data-key='+thisplugin+'][data-status]').show();
 										$('[data-key='+thisplugin+'][data-toggle-status]').show();
-										if(API.Helper.isSet(API,['Contents','Settings','plugins',thisplugin,'status'])){
+										if(Engine.Helper.isSet(Engine,['Contents','Settings','plugins',thisplugin,'status'])){
 											$('input[type="checkbox"][data-key="'+thisplugin+'"]').prop( "checked",dataset.output.settings[thisplugin].status);
 											$('input[type="checkbox"][data-key="'+thisplugin+'"]').bootstrapSwitch('state',dataset.output.settings[thisplugin].status);
 										} else { $('input[type="checkbox"][data-key="'+thisplugin+'"]').bootstrapSwitch('state',false); }
-										if(!API.Helper.isSet(dataset.output.settings,[thisplugin,'build'])||dataset.output.settings[thisplugin].build < manifest.build){
+										if(!Engine.Helper.isSet(dataset.output.settings,[thisplugin,'build'])||dataset.output.settings[thisplugin].build < manifest.build){
 											$('[data-key='+thisplugin+'][data-action="update"]').show();
 										}
 									}
@@ -103,11 +103,11 @@ API.Plugins.plugins = {
 						content.find('button[data-action]').off().click(function(){
 							switch($(this).attr('data-action')){
 								case'install':
-									API.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
+									Engine.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
 										json = JSON.parse(result);
 										if(json.success != undefined){
-											if(API.Helper.isSet(dataset,['output','settings',json.data.plugin,'status'])&&dataset.output.settings[json.data.plugin].status){
-												API.Plugins.plugins.Events.enable(json.data.plugin);
+											if(Engine.Helper.isSet(dataset,['output','settings',json.data.plugin,'status'])&&dataset.output.settings[json.data.plugin].status){
+												Engine.Plugins.plugins.Events.enable(json.data.plugin);
 											}
 											$('[data-key='+json.data.plugin+'][data-action="install"]').hide();
 											$('[data-key='+json.data.plugin+'][data-action="uninstall"]').show();
@@ -117,10 +117,10 @@ API.Plugins.plugins = {
 									});
 									break;
 								case'uninstall':
-									API.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
+									Engine.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
 										json = JSON.parse(result);
 										if(json.success != undefined){
-											API.Plugins.plugins.Events.disable(json.data.plugin);
+											Engine.Plugins.plugins.Events.disable(json.data.plugin);
 											$('[data-key='+json.data.plugin+'][data-action="install"]').show();
 											$('[data-key='+json.data.plugin+'][data-action="uninstall"]').hide();
 											$('[data-key='+json.data.plugin+'][data-status]').hide();
@@ -130,24 +130,24 @@ API.Plugins.plugins = {
 									});
 									break;
 								case'update':
-									API.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
+									Engine.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
 										json = JSON.parse(result);
 										if(json.success != undefined){
-											API.Plugins.plugins.Events.disable(json.data.plugin);
-											API.Plugins.plugins.Events.enable(json.data.plugin);
-											if(API.Helper.isSet(API.Plugins,[plugin,'update'])&&(typeof API.Plugins[plugin].update === 'function')){ API.Plugins[plugin].update(); }
+											Engine.Plugins.plugins.Events.disable(json.data.plugin);
+											Engine.Plugins.plugins.Events.enable(json.data.plugin);
+											if(Engine.Helper.isSet(Engine.Plugins,[plugin,'update'])&&(typeof Engine.Plugins[plugin].update === 'function')){ Engine.Plugins[plugin].update(); }
 											$('[data-key='+json.data.plugin+'][data-action="update"]').hide();
 										}
 									});
 									break;
 								case'updateAll':
 									$('button[data-action="update"]:visible').each(function(){
-										API.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
+										Engine.request('plugins',$(this).attr('data-action'),{data:{plugin:$(this).attr('data-key')}},function(result){
 											json = JSON.parse(result);
 											if(json.success != undefined){
-												API.Plugins.plugins.Events.disable(json.data.plugin);
-												API.Plugins.plugins.Events.enable(json.data.plugin);
-												if(API.Helper.isSet(API.Plugins,[plugin,'update'])&&(typeof API.Plugins[plugin].update === 'function')){ API.Plugins[plugin].update(); }
+												Engine.Plugins.plugins.Events.disable(json.data.plugin);
+												Engine.Plugins.plugins.Events.enable(json.data.plugin);
+												if(Engine.Helper.isSet(Engine.Plugins,[plugin,'update'])&&(typeof Engine.Plugins[plugin].update === 'function')){ Engine.Plugins[plugin].update(); }
 												$('[data-key='+json.data.plugin+'][data-action="update"]').hide();
 											}
 										});
@@ -157,15 +157,15 @@ API.Plugins.plugins = {
 						});
 						content.find('input[type="checkbox"]').each(function(){
 							plugin = $(this).attr('data-key');
-							if(API.Helper.isSet(API,['Contents','Settings','plugins',plugin,'status'])){
+							if(Engine.Helper.isSet(Engine,['Contents','Settings','plugins',plugin,'status'])){
 								$('input[type="checkbox"][data-key="'+plugin+'"]').prop( "checked",dataset.output.settings[plugin].status);
 								$(this).bootstrapSwitch({
 									state:dataset.output.settings[plugin].status,
 									onSwitchChange:function(e,state){
 										plugin = $(this).attr('data-key');
-										API.request('plugins','status',{data:{plugin:plugin,state:state}},function(){
-											if(state){ API.Plugins.plugins.Events.enable(plugin); }
-											else { API.Plugins.plugins.Events.disable(plugin); }
+										Engine.request('plugins','status',{data:{plugin:plugin,state:state}},function(){
+											if(state){ Engine.Plugins.plugins.Events.enable(plugin); }
+											else { Engine.Plugins.plugins.Events.disable(plugin); }
 										});
 									}
 								});
@@ -174,9 +174,9 @@ API.Plugins.plugins = {
 									state:false,
 									onSwitchChange:function(e,state){
 										plugin = $(this).attr('data-key');
-										API.request('plugins','status',{data:{plugin:plugin,state:state}},function(){
-											if(state){ API.Plugins.plugins.Events.enable(plugin); }
-											else { API.Plugins.plugins.Events.disable(plugin); }
+										Engine.request('plugins','status',{data:{plugin:plugin,state:state}},function(){
+											if(state){ Engine.Plugins.plugins.Events.enable(plugin); }
+											else { Engine.Plugins.plugins.Events.disable(plugin); }
 										});
 									}
 								});
@@ -189,9 +189,9 @@ API.Plugins.plugins = {
 	},
 	Events:{
 		enable:function(plugin){
-			API.request('api','getLanguage',{ toast: false, pace: false, required: true, }).then(function(result){
+			Engine.request('api','getLanguage',{ toast: false, pace: false, required: true, }).then(function(result){
 				var language = JSON.parse(result);
-				if(typeof language.error === 'undefined'){ API.Contents.Language = language.Lists.Language; }
+				if(typeof language.error === 'undefined'){ Engine.Contents.Language = language.Lists.Language; }
 				$('<link>').appendTo('head').attr({
 					type: 'text/css',
 					rel: 'stylesheet',
@@ -201,16 +201,16 @@ API.Plugins.plugins = {
 			});
 		},
 		disable:function(plugin){
-			API.request('api','getLanguage',{ toast: false, pace: false, required: true, }).then(function(result){
+			Engine.request('api','getLanguage',{ toast: false, pace: false, required: true, }).then(function(result){
 				var language = JSON.parse(result);
-				if(typeof language.error === 'undefined'){ API.Contents.Language = language.Lists.Language; }
-				if(API.Helper.isSet(API.Plugins,[plugin,'unload'])&&(typeof API.Plugins[plugin].unload === 'function')){ API.Plugins[plugin].unload(); }
+				if(typeof language.error === 'undefined'){ Engine.Contents.Language = language.Lists.Language; }
+				if(Engine.Helper.isSet(Engine.Plugins,[plugin,'unload'])&&(typeof Engine.Plugins[plugin].unload === 'function')){ Engine.Plugins[plugin].unload(); }
 				$('a[href^="?p='+plugin+'"]').remove();
 				$('link[href="/plugins/'+plugin+'/dist/css/styles.css"]').remove();
-				delete API.Plugins[plugin];
+				delete Engine.Plugins[plugin];
 			});
 		},
 	},
 }
 
-API.Plugins.plugins.init();
+Engine.Plugins.plugins.init();
